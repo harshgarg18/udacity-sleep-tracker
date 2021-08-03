f6947030affc4f699c4fc8b5a09e4795d11a764d
Harsh Garg <harsh.garg@juspay.in>
2021-08-03 22:58:37 +0530

    using DiffUtil.ItemCallback

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -67,7 +67,7 @@ class SleepTrackerFragment : Fragment() {
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, {
             it?.let {
-                adapter.data = it
+                adapter.submitList(it)
             }
         })
 
